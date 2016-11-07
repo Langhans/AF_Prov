ea72c6f4fd6beead6c998cd96e201b00d70c1f7e
Langhaus <timlanghans34@gmail.com>
2016-11-07 20:01:57 +0100

    index.html redone

--- public/controller.js
+++ public/controller.js
@@ -1,41 +1,50 @@
-
 var baseUrl = "http://localhost:3000/api";
 
 
-function getEmployeeList(){
+function refreshCompanySelector() {
 	$.ajax({
-		type:'GET',
-		url: baseUrl + "/employees",
+		type: 'GET',
+		url: baseUrl + "/companies",
 		datatype: 'json',
-		success: function(data){
-			console.log("Employees received");
-			$.each( data , function( index , employee){
-				$('#emp_list').append('<li>' + employee.name + '</li>');
+		success: function (data) {
+			console.log("Companies received");
+			$.each(data, function (index, company) {
+				var option_element = '<option value="' + company.id + '">' +
+					company.name + '</option>';
+				$('#comp_selector').append(option_element);
 			});
+			;
 		}
 	});
 }
 
 
-function addEmployee(){
+function addEmployee() {
 	var name_in = $("#emp_input").val();
-	console.log($("#emp_input").val());
-	var data = JSON.stringify({name: name_in});
+	var compId_in = $("#comp_selector").val();
+	console.log(name_in);
+	console.log(compId_in);
 
-	if (name && name.length > 1) {
-			$.ajax({
-				type: 'POST',
-				url: baseUrl + "/employees",
-				data: data,
-				success: function(){
-					alert("SUCCESS");
-					getEmployeeList();
-				},
-				error: function(error){
-					alert(error);
-				}
+	// minimal validation
+	if (name_in && String(name_in).length > 1 && compId_in) {
+		var data_out = {};
+		data_out.name = name_in;
+		data_out.companyId = compId_in;
+		console.log(JSON.stringify(data_out));
+
+		$.ajax({
+			type: 'POST',
+			url: baseUrl + "/employees",
+			data: JSON.stringify(data_out),
+			success: function () {
+				alert("Successfully added employee!");
+				refreshCompanySelector();
+			},
+			error: function (error) {
+				alert(error);
+			}
 		});
 	} else {
-		alert ("Name cannot be empty");
+		alert("Name cannot be empty");
 	}
 }
